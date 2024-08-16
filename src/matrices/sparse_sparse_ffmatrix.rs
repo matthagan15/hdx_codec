5ce5cf84819395a29cafc2d59eb755aa5ffa9145
Matthew Hagan <matthewhagan15@gmail.com>
2024-08-16 19:18:31 -0400

    sparse sparse matrix can do pivoting!

--- src/matrices/sparse_sparse_ffmatrix.rs
+++ src/matrices/sparse_sparse_ffmatrix.rs
@@ -50,25 +50,22 @@ impl SparseSparseFFMatrix {
         new
     }
 
-    pub fn row(&self, row_ix: usize) -> SparseVector {
-        todo!()
-    }
-
     /// Adds the entry to (row_ix, col_ix) in the matrix
     pub fn insert(&mut self, row_ix: usize, col_ix: usize, entry: FFRep) {
         let r = self
             .row_nodes
             .entry(row_ix)
-            .or_insert(self.hgraph.add_node(row_ix));
+            .or_insert_with(|| self.hgraph.add_node(row_ix));
         let c = self
             .col_nodes
             .entry(col_ix)
-            .or_insert(self.hgraph.add_node(col_ix));
+            .or_insert_with(|| self.hgraph.add_node(col_ix));
         let id = self
             .hgraph
             .find_id([*r, *c])
-            .or(Some(self.hgraph.add_edge([*r, *c], 0)))
+            .or_else(|| Some(self.hgraph.add_edge([*r, *c], 0)))
             .unwrap();
+
         let edge_mut = self.hgraph.get_edge_mut(&id).unwrap();
         *edge_mut += entry;
         *edge_mut %= self.field_mod;
@@ -90,7 +87,7 @@ impl SparseSparseFFMatrix {
         }
     }
 
-    pub fn query(&self, row_ix: usize, col_ix: usize) -> FF {
+    pub fn get(&self, row_ix: usize, col_ix: usize) -> FF {
         let r = self.row_nodes.get(&row_ix);
         let c = self.col_nodes.get(&col_ix);
         if r.is_none() || c.is_none() {
@@ -125,17 +122,23 @@ impl SparseSparseFFMatrix {
             .filter(|r| *r == usize::MAX)
     }
 
-    pub fn add_row_to_other(&mut self, source_row: usize, target_row: usize) {
+    pub fn add_scaled_row_to_target(
+        &mut self,
+        source_row: usize,
+        target_row: usize,
+        scalar: FFRep,
+    ) {
         let source_row_node = self.row_nodes.get(&source_row);
         if source_row_node.is_none() {
             return;
         }
         let source_row_node = source_row_node.unwrap();
         let link = self.hgraph.link_of_nodes([*source_row_node]);
+
         let t_row = self
             .row_nodes
             .entry(target_row)
-            .or_insert(self.hgraph.add_node(target_row));
+            .or_insert_with(|| self.hgraph.add_node(target_row));
         let mut edges_to_remove = Vec::new();
         for (edge_id, col_node_vec) in link {
             let source_entry = *self.hgraph.get_edge(&edge_id).unwrap();
@@ -143,31 +146,64 @@ impl SparseSparseFFMatrix {
             let target_edge_id = self
                 .hgraph
                 .find_id([*t_row, col_node])
-                .or(Some(self.hgraph.add_edge([*t_row, col_node], 0)))
+                .or_else(|| Some(self.hgraph.add_edge([*t_row, col_node], 0)))
                 .unwrap();
             let e = self.hgraph.get_edge_mut(&target_edge_id).unwrap();
-            println!("{:}", "*".repeat(50));
-            dbg!(&edge_id);
-            dbg!(&col_node_vec);
-            dbg!(&source_entry);
-            dbg!(&col_node);
-            dbg!(&target_edge_id);
-            dbg!(&e);
-            *e = (*e + source_entry) % self.field_mod;
+            *e = (*e + (source_entry * scalar)) % self.field_mod;
             if *e == 0 {
                 edges_to_remove.push(target_edge_id);
             }
-            dbg!(&e);
         }
         for id in edges_to_remove.into_iter() {
             self.hgraph.remove_edge(id);
+        }
+        if self.hgraph.containing_edges_of_nodes([*t_row]).len() == 0 {
+            self.hgraph.remove_node(*t_row);
+            self.row_nodes.remove(&target_row);
+        }
+    }
+
+    pub fn scale_row(&mut self, row_ix: usize, scalar: FFRep) {
+        let row_node = self.row_nodes.get(&row_ix);
+        if row_node.is_none() {
+            return;
+        }
+        let row_node = row_node.unwrap();
+        let edges = self.hgraph.containing_edges_of_nodes([*row_node]);
+        for edge_id in edges {
+            let e = self.hgraph.get_edge_mut(&edge_id).unwrap();
+            *e = (*e * scalar) % self.field_mod;
         }
     }
 
     /// Returns the column of the successfully pivotized row, or None if
     /// the row is empty.
-    pub fn pivotize_row(&mut self, row_ix: &usize) -> Option<usize> {
-        todo!()
+    pub fn pivotize_row(&mut self, row_ix: usize) -> Option<usize> {
+        let row_node = self.row_nodes.get(&row_ix);
+        if row_node.is_none() {
+            return None;
+        }
+        let row_node = *row_node.unwrap();
+        let link = self.hgraph.link_of_nodes([row_node]);
+        let (id, col_nodes_vec) = link.first().unwrap();
+        let pivot_col_node = col_nodes_vec[0];
+        let pivot_entry_pre_scaling = self.hgraph.get_edge(id).unwrap();
+        let scalar = FF::new(*pivot_entry_pre_scaling, self.field_mod)
+            .modular_inverse()
+            .0;
+        self.scale_row(row_ix, scalar);
+        let rows_link = self.hgraph.link_of_nodes([pivot_col_node]);
+        for (edge_id, row_node_vec) in rows_link {
+            let target_row_node = row_node_vec[0];
+            if target_row_node == row_node {
+                continue;
+            }
+            let target_entry = self.hgraph.get_edge(&edge_id).unwrap();
+            let scalar = FF::new(*target_entry, self.field_mod).modular_inverse().0;
+            let target_row_ix = self.hgraph.get_node(&target_row_node).unwrap();
+            self.add_scaled_row_to_target(row_ix, *target_row_ix, scalar);
+        }
+        Some(*self.hgraph.get_node(&pivot_col_node).unwrap())
     }
 
     pub fn get_row(&self, row_ix: &usize) -> Vec<(usize, FFRep)> {
@@ -226,9 +262,19 @@ mod tests {
         println!("{:?}", m.get_row(&0));
         m.insert(1, 3, 3);
         m.print();
-        dbg!(&m.hgraph);
-        m.add_row_to_other(0, 1);
+        m.add_scaled_row_to_target(0, 1, 2);
         m.print();
-        dbg!(m);
+        m.scale_row(0, 3);
+        m.print();
+        for ix in 2..7 {
+            m.add_scaled_row_to_target(0, ix, ix as u32);
+        }
+        m.print();
+        println!("pivot 0");
+        println!("col: {:?}", m.pivotize_row(0));
+        m.print();
+        println!("pivot 1");
+        println!("col: {:?}", m.pivotize_row(1));
+        m.print();
     }
 }
